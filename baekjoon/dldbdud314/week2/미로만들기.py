3a402ee2b905a15fb1340021eaf57ce52baae167
dldbdud314 <dldbdud314@naver.com>
2023-01-02 20:02:02 +0900

    미로만들기

---
+++ baekjoon/dldbdud314/week2/미로만들기.py
@@ -0,0 +1,58 @@
+dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]  # 아래, 왼, 위, 오른
+
+
+def turn_right(idx):  # 오른쪽으로 방향 전환
+    if idx + 1 >= 4:
+        idx = 0
+    else:
+        idx += 1
+    return idx
+
+
+def turn_left(idx):  # 왼쪽으로 방향 전환
+    if idx - 1 < 0:
+        idx = 3
+    else:
+        idx -= 1
+    return idx
+
+
+n = int(input())
+moves = input()
+
+# 미로 이동하며 표시
+area = [['-'] * 101 for _ in range(101)]
+
+x, y = 50, 50
+area[x][y] = '.'  # 시작점
+positions = [(x, y)]  # 이동 위치 기록
+idx = 0
+dx, dy = dirs[idx]  # 이동할 방향 : default 남쪽
+for move in moves:
+    if move == 'R':
+        idx = turn_right(idx)
+        dx, dy = dirs[idx]
+    elif move == 'L':
+        idx = turn_left(idx)
+        dx, dy = dirs[idx]
+    elif move == 'F':
+        x += dx
+        y += dy
+        area[x][y] = '.'
+        positions.append((x, y))
+
+# 왼쪽 시작점, 오른쪽 끝점 구분하기
+min_x, min_y = float('inf'), float('inf')
+max_x, max_y = -float('inf'), -float('inf')
+for xx, yy in positions:
+    min_x, min_y = min(min_x, xx), min(min_y, yy)
+    max_x, max_y = max(max_x, xx), max(max_y, yy)
+
+# 결과 출력
+for i in range(min_x, max_x + 1):
+    for j in range(min_y, max_y + 1):
+        if area[i][j] != '.':
+            print('#', end = '')
+        else:
+            print(area[i][j], end = '')
+    print()
