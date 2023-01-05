92b31568526f4e463d15ee39799582eb17186f43
dldbdud314 <dldbdud314@naver.com>
2023-01-05 12:52:20 +0900

    양팔저울_dfs로수정

--- baekjoon/dldbdud314/week2/양팔저울.py
+++ baekjoon/dldbdud314/week2/양팔저울.py
@@ -1,18 +1,36 @@
+'''
+DFS + DP
+
+DFS -> 구슬이 왼쪽에 고정 되어 있다고 했을 때,
+- 해당 추를 왼쪽 저울에 놓는다
+- 해당 추를 놓지 않는다
+- 해당 추를 오른쪽 저울에 놓는다
+
+cnt개의 추를 놨을 떄, weight 그램을 만들 수 있음 -> DP[cnt][weight] = True
+'''
+
 n = int(input())
 weights = list(map(int, input().split()))
 m = int(input())
 marbles = list(map(int, input().split()))
 
-dp = [[False] * 40001 for _ in range(n + 1)]
 
-for i in range(1, n + 1):
-    for j in range(max(marbles) + 1):
-        if j == 0:
-            dp[i][j] = True
-            continue
-        w = weights[i - 1]
-        dp[i][j] = dp[i - 1][abs(j - w)] or dp[i - 1][j + w] or dp[i - 1][j]
+# n개로 만들 수 있는 무게 반영 -> DP
+def dfs(cnt, weight):
+    #종료 조건
+    if dp[cnt][weight] or weight > 40000:
+        return
 
+    dp[cnt][weight] = True
+
+    if cnt < n:
+        dfs(cnt + 1, weight + weights[cnt])
+        dfs(cnt + 1, weight)
+        dfs(cnt + 1, abs(weight - weights[cnt]))
+
+
+dp = [[False] * 40001 for _ in range(31)]  # n개의 추로, k 그램을 만들 수 있는가?: DP[n][k]
+dfs(0, 0)
 for marble in marbles:
     if dp[n][marble]:
         print('Y', end=' ')
