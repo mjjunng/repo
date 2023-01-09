fe7a330059ed52f99afebff88432b361e4a54851
dragonappear <dragonappear1@gmail.com>
2023-01-09 20:16:55 +0900

    add:양과늑대

--- programmers/dragonappear/week3/양과늑대.py
+++ programmers/dragonappear/week3/양과늑대.py
@@ -1,1 +1,37 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92343
+from typing import List
+from collections import defaultdict
+import sys
+# DFS 완전탐색
+mx = -sys.maxsize
+def solution(info, edges):
+
+    def dfs(cur_node:int,sheep_count:int,wolf_count:int,visitable:List,visited:List)->None:
+        global mx
+
+        if visited[cur_node]: return # 현재 노드를 방문한 경우 return
+        else: visited[cur_node] = True # 현재 노드 방문 기록
+
+        if info[cur_node]: # 현재 노드 == 늑대
+            wolf_count += 1 # 늑대 카운트
+            if wolf_count >= sheep_count: 
+                return
+        else: # 현재 노드 == 양
+            sheep_count += 1 # 양 카운트
+            mx = max(mx,sheep_count) # 양 마리 업데이트
+
+        visitable.extend(binary_tree[cur_node]) 
+
+        for next in visitable:
+            dfs(next,sheep_count,wolf_count, [ n for n in visitable if n != next and not visited[n]],visited[:])
+        
+        
+    visited = [False] * len(info) # 노드 방문 체크 변수 초기화
+    binary_tree = defaultdict(list) # 트리 변수
+    for src,dst in edges: # 트리 초기화
+        binary_tree[src].append(dst)
+
+    # dfs
+    dfs(0,0,0,[],visited) # root node index , sheep_count, wolf_count , next_nodes , visited
+
+    return mx
--- programmers/dragonappear/week3/오픈채팅방.py
+++ programmers/dragonappear/week3/오픈채팅방.py
@@ -4,13 +4,13 @@ from collections import deque,defaultdict
 
 # 해시테이블을 활용한 풀이
 # 시간복잡도: O(n), n is the number of records
-# 시간복잡도: O(n), n is the number of records 
+# 공간복잡도: O(n), n is the number of records 
 def solution(record:List) -> List:
     answer = []
 
-    trace = []
-    uid_nickname_mapping = defaultdict(str)
-    in_out_mapping = {"IN":"님이 들어왔습니다.","OUT":"님이 나갔습니다."}
+    trace:List = [] # element: [uid,in_out_type]
+    uid_nickname_map = defaultdict(str) # uid:nickname
+    in_out_map = {"IN":"님이 들어왔습니다.","OUT":"님이 나갔습니다."} # static
 
     # O(n)
     for r in record:
@@ -22,16 +22,16 @@ def solution(record:List) -> List:
             cmd_type,uid = cmd[0],cmd[1]
         # O(1)
         if cmd_type == "Enter":
-            uid_nickname_mapping[uid] = nickname
+            uid_nickname_map[uid] = nickname
             trace.append([uid,"IN"])
         elif cmd_type == "Leave":
             trace.append([uid,"OUT"])
         else:
-            uid_nickname_mapping[uid] = nickname
+            uid_nickname_map[uid] = nickname
     
     # O(n)
-    for t in trace:
-        answer.append(uid_nickname_mapping[t[0]] + in_out_mapping[t[1]])
+    for uid,in_out in trace:
+        answer.append(uid_nickname_map[uid] + in_out_map[in_out])
 
     return answer
 
