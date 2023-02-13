b6d15fb1e249ab576d88df5ce693da16e1027b92
dldbdud314 <dldbdud314@naver.com>
2023-02-13 13:58:32 +0900

    FIX: PASSX_인사고과

--- programmers/dldbdud314/week7/인사고과.py
+++ programmers/dldbdud314/week7/인사고과.py
@@ -1,41 +1,36 @@
 """
 O(n^2)이 아닌 접근 방식 안 떠올라서 풀이 참고
 -> sort(key = lambda x: (-x[0], x[1])) 하고 비교
+48.0 / 100.0 FAIL
 
-48.0 / 100.0
+수정: 완호 기준 점수 높은 사람 카운트
+76.0 / 100.0 FAIL
 """
 
 
 def solution(scores):
-    scores = list(map(tuple, scores))
-    wh = scores[0]  # 원호 점수 저장
+    # 완호 점수
+    wh = scores[0]
+    wh_sum = sum(scores[0])
+    scores = scores[1:]  # 완호 제외
+    # 인센티브 못 받는 사람 가리기 위해
     scores.sort(key=lambda x: (-x[0], x[1]))
 
-    # 인센티브 못 받는 점수 제거
-    to_remove = set()  # 인센티브 못 받는 점수 목록
-    for i in range(1, len(scores)):
-        a1, b1 = scores[i - 1]
-        a2, b2 = scores[i]
-        if a1 > a2 and b1 > b2:
-            to_remove.add(scores[i])
+    # 완호가 인센 못 받는지
+    # 받는다면 본인보다 점수 크고 && 인센 받는 사람 기준 RANKING
+    s1, s2 = -1, -1  # 인센 확인용 이전 점수 저장
+    rank = 1
+    for score in scores:
+        if wh[0] < score[0] and wh[1] < score[1]:
+            return -1
+        if s1 > score[0] and s2 > score[1]:  # 인센X -> 쓰루
+            s1, s2 = score[0], score[1]
+            continue
+        if score[0] + score[1] > wh_sum:  # 완호보다 점수 높은 사람 카운트
+            rank += 1
+        s1, s2 = score[0], score[1]
 
-    if wh in to_remove:  # 원호가 인센 못 받는 경우
-        return -1
-
-    scores = [score for score in scores if score not in to_remove]  # 제거하고
-
-    # RANKING - 동석차 고려
-    scores = sorted(scores, key=lambda x: -sum(x))  # 합 기준 내림차순 정렬
-    cur_rank = 1
-    if scores[0] == wh:
-        return cur_rank
-    for i in range(1, len(scores)):
-        sum1 = sum(scores[i - 1])
-        sum2 = sum(scores[i])
-        if sum1 > sum2:
-            cur_rank = i + 1
-        if scores[i] == wh:  # 원호 점수인 경우
-            return cur_rank
+    return rank
 
 
 print(solution([[3, 1], [1, 4], [2, 3], [2, 3], [1, 5], [1, 0], [1, 0]]))
