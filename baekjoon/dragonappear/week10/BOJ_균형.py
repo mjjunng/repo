bf2ac4e9fed9294693ebebb2fadf27f8195966cb
dragonappear <dragonappear1@gmail.com>
2023-03-02 23:05:49 +0900

    .

--- baekjoon/dragonappear/week10/BOJ_균형.py
+++ baekjoon/dragonappear/week10/BOJ_균형.py
@@ -7,23 +7,22 @@ input = stdin.readline
 MAX = int(log2(1000000000))*2
 d = [0]*(MAX)
 d[0], d[1], d[2] = 1, 2, 4
-for i in range(2, MAX):
+for i in range(3, MAX):
     d[i] = d[i-1]+d[i-2]+1
 
 for _ in range(int(input())):
     n = int(input())
-    idx = bisect_right(d, n)
-    print(idx)
+    print(bisect_right(d, n))
 
 """
 DP
 
-N(h) = 높이가 h인 AVL 트리 중 최소 노드 개수
-N(0)=1
-N(1)=2
-N(2)=4
-N(3)=7
+d(h) = 높이가 h인 AVL 트리 중 최소 노드 개수
+d(0)=1
+d(1)=2
+d(2)=4
+d(3)=7
 
--> N(h)=N(h-2)+N(h-1)+1
--> 높이가 H, 노드 개수가 N인 AVL 트리: h=O(logN)
+-> d(h) = d(h-2)+d(h-1)+1
+-> 높이가 H, 노드 개수가 N인 AVL 트리: H = O(logN)
 """
