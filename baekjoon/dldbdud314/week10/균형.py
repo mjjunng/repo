ac398ccf5546755f3b3065494d78578849402a18
dldbdud314 <dldbdud314@naver.com>
2023-03-03 13:57:27 +0900

    .

--- baekjoon/dldbdud314/week10/균형.py
+++ baekjoon/dldbdud314/week10/균형.py
@@ -1,10 +1,24 @@
 """
-22968. 균형
-TODO
+22968. 균형 - 풀이 참고
+
+** DP
+- dp[h] = dp[h-1] + dp[h-2] + 1
+- dp[h] : 높이가 h인 트리의 최대 노드 개수
 """
-# import sys
-# input = sys.stdin.readline
-#
-# t = int(input())
-# for _ in range(t):
-#     v = int(input())
+import sys
+from bisect import bisect_right
+input = sys.stdin.readline
+
+
+# 최소 노드 개수로 만들 수 있는 최대 높이 -> dp[h] = n (h = 높이, n = 노드 개수)
+dp = [1, 2]
+while True:
+    dp.append(dp[-1] + dp[-2] + 1)
+    if dp[-1] > 1_000_000_000:
+        dp.pop()
+        break
+
+t = int(input())
+for _ in range(t):
+    v = int(input())
+    print(bisect_right(dp, v))
